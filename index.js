fa68db2645d78bbafcdb5ced788ea0600aa2dbdf
DaisukeFFF <ifc.dai.faculty@gmail.com>
2018-02-21 23:52:58 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+process.env.API_BASE_URL = "http://localhost:8080/bot";
+const express = require('express');
+const line = require('@line/bot-sdk');
+
+const config = {
+    channelAccessToken: process.env.YOUR_CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.YOUR_CHANNEL_SECRET
+};
+
+const app = express();
+app.post('/', line.middleware(config), (req, res) => {
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result) => res.json(result));
+});
+
+const client = new line.Client(config);
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return Promise.resolve(null);
+    }
+
+    return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: event.message.text
+    });
+}
+
+app.listen(3000);
