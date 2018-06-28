bae58724cc70dfc4428fb5568eafe553168f193c
DaisukeFFF <ifc.dai.faculty@gmail.com>
2018-06-28 23:15:59 +0900

    add lib

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const linebot = require('@line/bot-sdk');
+const linebotHelper = require('./lib/responseMessages');
 const config = {
-    channelAccessToken: process.env.YOUR_CHANNEL_ACCESS_TOKEN,
-    channelSecret: process.env.YOUR_CHANNEL_SECRET
+    channelAccessToken: process.env.YOUR_CHANNEL_ACCESS_TOKEN || "",
+    channelSecret: process.env.YOUR_CHANNEL_SECRET || ""
 };
 const app = express();
 
 app.set('port', (process.env.PORT || 3030));
+
 app.post('/', linebot.middleware(config), (req, res) => {
     Promise
         .all(req.body.events.map(handleEvent))
         .then(result => res.json(result));
+});
+
+app.get('/test', function(req, res) {
+    const helper = new linebotHelper();
+    console.log('test ->', helper.getBubble());
+    res.json({result: true});
 });
 
 const client = new linebot.Client(config);
@@ -33,6 +41,7 @@ function handleEvent(event) {
 }
 
 function messageEvent() {
+    const helper = new linebotHelper();
     const {
         type,
         text,
@@ -43,74 +52,10 @@ function messageEvent() {
     }
 
     if (text.includes('疲れた') || text.includes('つかれた') || text.includes('ツカレタ')) {
-        return client.replyMessage(this.line.replyToken, {
-            "type": "image",
-            "originalContentUrl": "https://chikyu-jack.com/wp-content/uploads/2015/06/saddest_cat_13.jpg",
-            "previewImageUrl": "https://chikyu-jack.com/wp-content/uploads/2015/06/saddest_cat_13.jpg"
-        });
+        return client.replyMessage(this.line.replyToken, helper.sendImage("https://chikyu-jack.com/wp-content/uploads/2015/06/saddest_cat_13.jpg"));
     }
-    return client.replyMessage(this.line.replyToken, {
-        type: 'text',
-        text: text
-    });
+    return client.replyMessage(this.line.replyToken, helper.sendText(text));
 }
-//     return client.replyMessage(this.line.replyToken, {
-//         "type": "flex",
-//         "altText": "this is a flex message",
-//         "contents": {
-//             "type": "bubble",
-//             "styles": {
-//                 "header": {
-//                     "backgroundColor": "#ffaaaa"
-//                 },
-//                 "body": {
-//                     "backgroundColor": "#aaffaa"
-//                 },
-//                 "footer": {
-//                     "backgroundColor": "#aaaaff"
-//                 }
-//             },
-//             "header": {
-//                 "type": "box",
-//                 "layout": "vertical",
-//                 "contents": [
-//                     {
-//                         "type": "text",
-//                         "text": "header"
-//                     }
-//                 ]
-//             },
-//             "hero": {
-//                 "type": "image",
-//                 "url": "https://example.com/flex/images/image.jpg",
-//                 "size": "full",
-//                 "aspectRatio": "2:1"
-//             },
-//             "body": {
-//                 "type": "box",
-//                 "layout": "vertical",
-//                 "contents": [
-//                     {
-//                         "type": "text",
-//                         "text": "body"
-//                     }
-//                 ]
-//             },
-//             "footer": {
-//                 "type": "box",
-//                 "layout": "vertical",
-//                 "contents": [
-//                     {
-//                         "type": "text",
-//                         "text": "footer"
-//                     }
-//                 ]
-//             }
-//         }
-//     });
-//
-//
-// }
 
 function followEvent() {
     console.log('follow success!');
