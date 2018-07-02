f66e0f24aff585905c286112dde3efa7097bb929
DaisukeFFF <ifc.dai.faculty@gmail.com>
2018-07-03 02:39:08 +0900

    test

--- index.js
+++ index.js
@@ -64,10 +64,7 @@ async function messageEvent() {
     if (text.includes('item')) {
         const test = await helper.sendItems(text);
         console.log("items =>", test.body.contents[0].contents);
-        return client.replyMessage(this.line.replyToken, {
-            type: 'text',
-            text: text
-        });
+        return client.replyMessage(this.line.replyToken, test);
     }
 
 
