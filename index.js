496bd43952745dc9afeb0a8a351c2606d9fd9b75
DaisukeFFF <ifc.dai.faculty@gmail.com>
2018-07-05 20:46:56 +0900

    test

--- index.js
+++ index.js
@@ -66,11 +66,11 @@ async function messageEvent() {
         return client.replyMessage(this.line.replyToken, test);
     }
     if (text.includes('セパレータ')) {
-        const space = await helper.sendeparator(text);
+        const space = await helper.sendSeparator(text);
         return client.replyMessage(this.line.replyToken, space);
     }
-    if (text.includes('イメージ')) {
-        const space = await helper.sendImage(text);
+    if (text.includes('ボタン')) {
+        const space = await helper.sendButton(text);
         return client.replyMessage(this.line.replyToken, space);
     }
     if (text.includes('スペーサー')) {
